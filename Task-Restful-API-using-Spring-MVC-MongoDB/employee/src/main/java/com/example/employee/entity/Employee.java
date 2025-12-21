a68a4796793cf15808b06a3f9e11af200b54488c
MOHAMMED UVAIS MOHAJIR F <uvais2003@gmail.com>
2025-12-21 13:02:38 +0530

    Module-4 - Spring Data MongoDB (Restful API using Spring MVC & MongoDB)

---
+++ Task-Restful-API-using-Spring-MVC-MongoDB/employee/src/main/java/com/example/employee/entity/Employee.java
@@ -0,0 +1,18 @@
+package com.example.employee.entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "employees")
+public class Employee {
+    @Id
+    private int employeeId;
+    private String employeeName;
+    private String employeeEmail;
+    private String location;
+}
